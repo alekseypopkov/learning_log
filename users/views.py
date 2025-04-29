c152e25f266a528004a8febeb6925e95a88396c5
Tara Routray <dev@tararoutray.com>
2025-04-29 22:19:52 +1000

    new topic

--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
-def index(request):
-    """Главная страница приложения "Сино-корейские числительные - KOREAN NUMERALS."""
-    return render(request, 'sk_numerals_logs/index.html')
+def new_topic(request):
+    """Добавляет новую тему."""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
