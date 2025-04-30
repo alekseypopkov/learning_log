11681c591f1a10d53995b71e0cf508f916ea63a0
Alexey Popkov <alvlpv969@gmail.com>
2025-04-30 13:36:51 +1000

    topic and entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,8 +6,16 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Главная страница
     path('', views.index, name='index'),
+
     # Страница со списком всех тем.
     path('topics/', views.topics, name='topics'),
+    
     # Страница с подробной информацией по отдельной теме.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    
+    # Страница для добавления новой темы.
+    path('new_topic/', views.new_topic, name='new_topic'),
+    
+    # Страница для добавления новой записи.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,17 +1,13 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
 def index(request):
     """Главная страница приложения "Журнал обучения"."""
     return render(request, 'index.html')
-
-# def index(request):
-#   template = loader.get_template('index.html')
-#   return HttpResponse(template.render())
 
 def topics(request):
     """Выводит список тем."""
@@ -25,3 +21,36 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Добавляет новую тему."""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = EntryForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    # Вывести пустую или недействительную форму.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
